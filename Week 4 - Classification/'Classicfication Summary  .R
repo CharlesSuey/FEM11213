10b3044a198d6b69ca656a59f7fccd95902d2f63
CharlesSuey <149478149+CharlesSuey@users.noreply.github.com>
2023-12-22 11:51:04 +0100

    Add files via upload

---
+++ Week 4 - Classification/'Classicfication Summary  .R
@@ -0,0 +1,21 @@
+'Classicfication Summary 
+
+**Classification using KNN Algorithm with `MASS` and `class` libraries:**
+    - Applied KNN algorithm to the `fgl` dataset.
+    - Scaled the features and split the dataset into training and testing sets.
+    - Demonstrated a classification example using the `gamlr` library and the `credit.csv` dataset.
+    - Used sparse matrix representation for the design matrix.
+    - Applied LASSO penalized multinomial regression using the `glmnet` package 
+
+**Classification Example Metrics:**
+    - Calculated various metrics such as min, AICc, AIC, and out-of-sample R^2 in classification example.
+    - Investigated false positive and false negative rates.
+
+**Extension: LASSO Penalized Multinomial Regression Example:**
+    - Building from a suggestion from ChatGPT
+    - Installed and loaded the `glmnet` package.
+    - Created a sparse design matrix for the `fgl` dataset.
+    - Performed cross-validated LASSO penalized multinomial regression.
+    - Plotted the results and explored the coefficients and class probabilities.
+
+#For the Extension I put the code that we had run in slides up to the extension due to mind blank I asked ChatGPT and asked how I could extend the provided cod, using ChatGPT I was able to come up with the extenstion as it help guide me on what to do for the extenstion. 
